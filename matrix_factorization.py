33b3f00f8768692818832f63d879025211abad8b
Zifeng Wang <wangzf18@mails.tsinghua.edu.cn>
2020-04-07 19:36:24 +0800

    ncf

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -418,6 +418,480 @@ class MF_SNIPS(nn.Module):
         return one_over_zl
 
 
+"""Neural Collaborative Filtering based methods.
+"""
+
+class NCF(nn.Module):
+    """The neural collaborative filtering method.
+    """
+    def __init__(self, num_users, num_items, embedding_k=4):
+        super(NCF, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.W = torch.nn.Embedding(self.num_users, self.embedding_k)
+        self.H = torch.nn.Embedding(self.num_items, self.embedding_k)
+        self.linear_1 = torch.nn.Linear(self.embedding_k*2, self.embedding_k)
+        self.relu = torch.nn.ReLU()
+        self.linear_2 = torch.nn.Linear(self.embedding_k, 1, bias=False)
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.xent_func = torch.nn.BCELoss()
+
+
+    def forward(self, x, is_training=False):
+        user_idx = torch.LongTensor(x[:,0])
+        item_idx = torch.LongTensor(x[:,1])
+        U_emb = self.W(user_idx)
+        V_emb = self.H(item_idx)
+
+        # concat
+        z_emb = torch.cat([U_emb, V_emb], axis=1)
+
+        h1 = self.linear_1(z_emb)
+        h1 = self.relu(h1)
+
+        out = self.linear_2(h1)
+
+        # out = torch.sum(U_emb.mul(V_emb), 1)
+
+        if is_training:
+            return out, U_emb, V_emb
+        else:
+            return out
+
+    def fit(self, x, y, num_epoch=1000, lr=0.05, lamb=0, tol=1e-4, batch_size=128, verbose=0):
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lamb)
+        last_loss = 1e9
+
+        num_sample = len(x)
+        total_batch = num_sample // batch_size
+
+        for epoch in range(num_epoch):
+            all_idx = np.arange(num_sample)
+            np.random.shuffle(all_idx)
+            epoch_loss = 0
+            for idx in range(total_batch):
+                # mini-batch training
+                selected_idx = all_idx[batch_size*idx:(idx+1)*batch_size]
+                sub_x = x[selected_idx]
+                sub_y = y[selected_idx]
+
+                optimizer.zero_grad()
+                pred, u_emb, v_emb = self.forward(sub_x, True)
+
+                pred = self.sigmoid(pred)
+
+                xent_loss = self.xent_func(pred, torch.unsqueeze(torch.Tensor(sub_y),1))
+
+                loss = xent_loss
+                loss.backward()
+                optimizer.step()
+                epoch_loss += xent_loss.detach().numpy()
+
+            relative_loss_div = (last_loss-epoch_loss)/(last_loss+1e-10)
+            if  relative_loss_div < tol:
+                print("[NCF] epoch:{}, xent:{}".format(epoch, epoch_loss))
+                break
+                
+            last_loss = epoch_loss
+
+            if epoch % 10 == 0 and verbose:
+                print("[NCF] epoch:{}, xent:{}".format(epoch, epoch_loss))
+
+            if epoch == num_epoch - 1:
+                print("[Warning] Reach preset epochs, it seems does not converge.")
+
+    def partial_fit(self, x, y, num_epoch=1000, lr=0.05, lamb=0, tol=1e-4):
+        self.fit(x, y, num_epoch=1000, lr=0.05, lamb=0, tol=1e-4)
+
+    def predict(self, x):
+        pred = self.forward(x)
+        pred = self.sigmoid(pred)
+        return pred.detach().numpy().flatten()
+
+    def predict_proba(self, x):
+        pred = self.forward(x)
+        pred = pred.reshape(-1,1)
+        pred = self.sigmoid(pred)
+        return np.concatenate([1-pred,pred],axis=1)
+
+
+class NCF_IPS(nn.Module):
+    """The neural collaborative filtering method.
+    """
+    def __init__(self, num_users, num_items, embedding_k=4):
+        super(NCF_IPS, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.W = torch.nn.Embedding(self.num_users, self.embedding_k)
+        self.H = torch.nn.Embedding(self.num_items, self.embedding_k)
+        self.linear_1 = torch.nn.Linear(self.embedding_k*2, self.embedding_k)
+        self.relu = torch.nn.ReLU()
+        self.linear_2 = torch.nn.Linear(self.embedding_k, 1, bias=False)
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.xent_func = torch.nn.BCELoss()
+
+
+    def forward(self, x, is_training=False):
+        user_idx = torch.LongTensor(x[:,0])
+        item_idx = torch.LongTensor(x[:,1])
+        U_emb = self.W(user_idx)
+        V_emb = self.H(item_idx)
+
+        # concat
+        z_emb = torch.cat([U_emb, V_emb], axis=1)
+
+        h1 = self.linear_1(z_emb)
+        h1 = self.relu(h1)
+
+        out = self.linear_2(h1)
+
+        # out = torch.sum(U_emb.mul(V_emb), 1)
+
+        if is_training:
+            return out, U_emb, V_emb
+        else:
+            return out
+
+    def fit(self, x, y, y_ips=None,
+        num_epoch=1000, batch_size=128, 
+        lr=0.05, lamb=0, tol=1e-4, verbose=0):
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lamb)
+        last_loss = 1e9
+
+        num_sample = len(x)
+        total_batch = num_sample // batch_size
+
+        if y_ips is None:
+            one_over_zl = self._compute_IPS(x, y)
+        else:
+            one_over_zl = self._compute_IPS(x, y, y_ips)
+
+        for epoch in range(num_epoch):
+            all_idx = np.arange(num_sample)
+            np.random.shuffle(all_idx)
+            epoch_loss = 0
+            for idx in range(total_batch):
+                # mini-batch training
+                selected_idx = all_idx[batch_size*idx:(idx+1)*batch_size]
+                sub_x = x[selected_idx]
+                sub_y = torch.Tensor(y[selected_idx])
+
+                # propensity score
+                inv_prop = one_over_zl[selected_idx]
+                pred, u_emb, v_emb = self.forward(sub_x, True)
+
+                pred = self.sigmoid(pred)
+
+                xent_loss = F.binary_cross_entropy(torch.squeeze(pred), sub_y,
+                    weight=inv_prop)
+
+                loss = xent_loss
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                epoch_loss += xent_loss.detach().numpy()
+
+            relative_loss_div = (last_loss-epoch_loss)/(last_loss+1e-10)
+            if  relative_loss_div < tol:
+                print("[NCF-IPS] epoch:{}, xent:{}".format(epoch, epoch_loss))
+                break
+                
+            last_loss = epoch_loss
+
+            if epoch % 10 == 0 and verbose:
+                print("[NCF-IPS] epoch:{}, xent:{}".format(epoch, epoch_loss))
+
+            if epoch == num_epoch - 1:
+                print("[NCF-IPS] Reach preset epochs, it seems does not converge.")
+
+
+    def predict(self, x):
+        pred = self.forward(x)
+        pred = self.sigmoid(pred)
+        return pred.detach().numpy().flatten()
+
+    def predict_proba(self, x):
+        pred = self.forward(x)
+        pred = pred.reshape(-1,1)
+        pred = self.sigmoid(pred)
+        return np.concatenate([1-pred,pred],axis=1)
+
+    def _compute_IPS(self,x,y,y_ips=None):
+        if y_ips is None:
+            one_over_zl = np.ones(len(y))
+        else:
+            py1 = y_ips.sum() / len(y_ips)
+            py0 = 1 - py1
+            po1 = len(x) / (x[:,0].max() * x[:,1].max())
+            py1o1 = y.sum() / len(y)
+            py0o1 = 1 - py1o1
+
+            propensity = np.zeros(len(y))
+
+            propensity[y == 0] = (py0o1 * po1) / py0
+            propensity[y == 1] = (py1o1 * po1) / py1
+            one_over_zl = 1 / propensity
+
+        one_over_zl = torch.Tensor(one_over_zl)
+        return one_over_zl
+
+
+class NCF_SNIPS(nn.Module):
+    """The neural collaborative filtering method.
+    """
+    def __init__(self, num_users, num_items, embedding_k=4):
+        super(NCF_SNIPS, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.W = torch.nn.Embedding(self.num_users, self.embedding_k)
+        self.H = torch.nn.Embedding(self.num_items, self.embedding_k)
+        self.linear_1 = torch.nn.Linear(self.embedding_k*2, self.embedding_k)
+        self.relu = torch.nn.ReLU()
+        self.linear_2 = torch.nn.Linear(self.embedding_k, 1, bias=False)
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.xent_func = torch.nn.BCELoss()
+
+
+    def forward(self, x, is_training=False):
+        user_idx = torch.LongTensor(x[:,0])
+        item_idx = torch.LongTensor(x[:,1])
+        U_emb = self.W(user_idx)
+        V_emb = self.H(item_idx)
+
+        # concat
+        z_emb = torch.cat([U_emb, V_emb], axis=1)
+
+        h1 = self.linear_1(z_emb)
+        h1 = self.relu(h1)
+
+        out = self.linear_2(h1)
+
+        # out = torch.sum(U_emb.mul(V_emb), 1)
+
+        if is_training:
+            return out, U_emb, V_emb
+        else:
+            return out
+
+    def fit(self, x, y, y_ips=None,
+        num_epoch=1000, batch_size=128, 
+        lr=0.05, lamb=0, tol=1e-4, verbose=0):
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lamb)
+        last_loss = 1e9
+
+        num_sample = len(x)
+        total_batch = num_sample // batch_size
+
+        if y_ips is None:
+            one_over_zl = self._compute_IPS(x, y)
+        else:
+            one_over_zl = self._compute_IPS(x, y, y_ips)
+
+        for epoch in range(num_epoch):
+            all_idx = np.arange(num_sample)
+            np.random.shuffle(all_idx)
+            epoch_loss = 0
+            for idx in range(total_batch):
+                # mini-batch training
+                selected_idx = all_idx[batch_size*idx:(idx+1)*batch_size]
+                sub_x = x[selected_idx]
+                sub_y = torch.Tensor(y[selected_idx])
+
+                # propensity score
+                inv_prop = one_over_zl[selected_idx]
+                sum_inv_prop = torch.sum(inv_prop)
+
+                pred, u_emb, v_emb = self.forward(sub_x, True)
+
+                pred = self.sigmoid(pred)
+
+                loss = F.binary_cross_entropy(torch.squeeze(pred), sub_y,
+                    weight=inv_prop,reduction="sum")
+                loss = loss / sum_inv_prop
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                epoch_loss += loss.detach().numpy()
+
+            relative_loss_div = (last_loss-epoch_loss)/(last_loss+1e-10)
+            if  relative_loss_div < tol:
+                print("[NCF-SNIPS] epoch:{}, xent:{}".format(epoch, epoch_loss))
+                break
+                
+            last_loss = epoch_loss
+
+            if epoch % 10 == 0 and verbose:
+                print("[NCF-SNIPS] epoch:{}, xent:{}".format(epoch, epoch_loss))
+
+            if epoch == num_epoch - 1:
+                print("[NCF-SNIPS] Reach preset epochs, it seems does not converge.")
+
+
+    def predict(self, x):
+        pred = self.forward(x)
+        pred = self.sigmoid(pred)
+        return pred.detach().numpy().flatten()
+
+    def predict_proba(self, x):
+        pred = self.forward(x)
+        pred = pred.reshape(-1,1)
+        pred = self.sigmoid(pred)
+        return np.concatenate([1-pred,pred],axis=1)
+
+    def _compute_IPS(self,x,y,y_ips=None):
+        if y_ips is None:
+            one_over_zl = np.ones(len(y))
+        else:
+            py1 = y_ips.sum() / len(y_ips)
+            py0 = 1 - py1
+            po1 = len(x) / (x[:,0].max() * x[:,1].max())
+            py1o1 = y.sum() / len(y)
+            py0o1 = 1 - py1o1
+
+            propensity = np.zeros(len(y))
+
+            propensity[y == 0] = (py0o1 * po1) / py0
+            propensity[y == 1] = (py1o1 * po1) / py1
+            one_over_zl = 1 / propensity
+
+        one_over_zl = torch.Tensor(one_over_zl)
+        return one_over_zl
+
+class NCF_VITA(nn.Module):
+    """The neural collaborative filtering method.
+    """
+    def __init__(self, num_users, num_items, embedding_k=4):
+        super(NCF_VITA, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.W = torch.nn.Embedding(self.num_users, self.embedding_k)
+        self.H = torch.nn.Embedding(self.num_items, self.embedding_k)
+        self.linear_1 = torch.nn.Linear(self.embedding_k*2, self.embedding_k)
+        self.relu = torch.nn.ReLU()
+        self.linear_2 = torch.nn.Linear(self.embedding_k, 1, bias=False)
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.xent_func = torch.nn.BCELoss()
+
+
+    def forward(self, x, is_training=False):
+        user_idx = torch.LongTensor(x[:,0])
+        item_idx = torch.LongTensor(x[:,1])
+        U_emb = self.W(user_idx)
+        V_emb = self.H(item_idx)
+
+        # concat
+        z_emb = torch.cat([U_emb, V_emb], axis=1)
+
+        h1 = self.linear_1(z_emb)
+        h1 = self.relu(h1)
+
+        out = self.linear_2(h1)
+
+        # out = torch.sum(U_emb.mul(V_emb), 1)
+
+        if is_training:
+            return out, U_emb, V_emb
+        else:
+            return out
+
+    def fit(self, x, y, num_epoch=1000, batch_size=128,
+            lr=0.05, lamb=1e-4,
+            alpha=0.1, gamma = 0.01, tol=1e-4, verbose=0):
+
+        self.alpha = alpha
+        self.gamma = gamma
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lamb)
+        
+        last_loss = 1e9
+
+        # generate all counterfactuals and factuals for info reg
+        x_all = generate_total_sample(self.num_users, self.num_items)
+
+        num_sample = len(x)
+        total_batch = num_sample // batch_size
+
+        for epoch in range(num_epoch):
+            all_idx = np.arange(num_sample)
+            np.random.shuffle(all_idx)
+
+            # sampling counterfactuals
+            ul_idxs = np.arange(x_all.shape[0])
+            np.random.shuffle(ul_idxs)
+
+            epoch_loss = 0
+
+            for idx in range(total_batch):
+                # mini-batch training
+                selected_idx = all_idx[batch_size*idx:(idx+1)*batch_size]
+                sub_x = x[selected_idx]
+                sub_y = y[selected_idx]
+                sub_y = torch.Tensor(sub_y)
+
+                pred, u_emb, v_emb = self.forward(sub_x, True)
+
+                pred = self.sigmoid(pred)
+
+                xent_loss = self.xent_func(torch.squeeze(pred), sub_y)
+
+                # pair wise loss
+                x_sampled = x_all[ul_idxs[idx* batch_size:(idx+1)*batch_size]]
+
+                pred_ul,_,_ = self.forward(x_sampled, True)
+                pred_ul = self.sigmoid(pred_ul)
+                
+                logp_hat = F.log_softmax(pred, dim=0)
+                info_loss = -torch.mean(pred_ul * logp_hat + (1-pred_ul)*F.log_softmax(1-pred,dim=0))
+                
+                # info_loss = -torch.mean(pred_ul * logp_hat)
+                ent_loss = -torch.mean(pred * logp_hat)
+
+                loss = xent_loss + alpha * info_loss - gamma * ent_loss
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += xent_loss.detach().numpy()
+
+            relative_loss_div = (last_loss-epoch_loss)/(last_loss+1e-10)
+            
+            if  relative_loss_div < tol:
+                print("[NCF-VITA] epoch:{}, xent:{}".format(epoch, epoch_loss))
+                break
+                
+            last_loss = epoch_loss
+
+            if epoch % 10 == 0 and verbose:
+                print("[NCF-VITA] epoch:{}, xent:{}".format(epoch, epoch_loss))
+
+            if epoch == num_epoch - 1:
+                print("[NCF-VITA] Reach preset epochs, it seems does not converge.")
+
+    def predict(self, x):
+        pred = self.forward(x)
+        pred = self.sigmoid(pred)
+        return pred.detach().numpy().flatten()
+
+    def predict_proba(self, x):
+        pred = self.forward(x)
+        pred = pred.reshape(-1,1)
+        pred = self.sigmoid(pred)
+        return np.concatenate([1-pred,pred],axis=1)
+
+
+
+
 def one_hot(x):
     out = torch.cat([torch.unsqueeze(1-x,1),torch.unsqueeze(x,1)],axis=1)
     return out
--- test.py
+++ test.py
@@ -9,6 +9,9 @@ import pdb
 
 from dataset import load_data
 from matrix_factorization import MF, MF_VITA, MF_IPS, MF_SNIPS
+
+from matrix_factorization import NCF, NCF_VITA, NCF_IPS, NCF_SNIPS
+
 from utils import gini_index, get_user_wise_ctr, rating_mat_to_sample, binarize, shuffle, minU
 mse_func = lambda x,y: np.mean((x-y)**2)
 acc_func = lambda x,y: np.sum(x == y) / len(x)
@@ -36,129 +39,83 @@ y_train = binarize(y_train)
 y_test = binarize(y_test)
 
 
-"MF-SNIPS"
-mf_snips = MF_SNIPS(num_user, num_item)
-
-ips_idxs = np.arange(len(y_test))
-np.random.shuffle(ips_idxs)
-y_ips = y_test[ips_idxs[:int(0.05 * len(ips_idxs))]]
-
-mf_snips.fit(x_train, y_train, y_ips=y_ips,lr=0.05,
-        batch_size=2048, lamb=1e-5,
-        verbose=1)
-
-test_pred = mf_snips.predict(x_test)
-mse_mfsnips = mse_func(y_test, test_pred)
-auc_mfsnips = roc_auc_score(y_test, test_pred)
-print("***"*5 + "[MF-SNIPS]" + "***"*5)
-print("[MF-SNIPS] test mse:", mse_mfsnips)
-print("[MF-SNIPS] test auc:", auc_mfsnips)
-user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
-gi,gu = gini_index(user_wise_ctr)
-print("***"*5 + "[MF-SNIPS]" + "***"*5)
-
-
-# "MF naive"
-# mf = MF(num_user, num_item)
-# mf.fit(x_train, y_train, lr=0.01, batch_size=2048, lamb=1e-4,
-#     verbose=1)
-# test_pred = mf.predict(x_test)
-# mse_mf = mse_func(y_test, test_pred)
-# auc_mf = roc_auc_score(y_test, test_pred)
-# print("***"*5 + "[MF]" + "***"*5)
-# print("[MF] test mse:", mse_func(y_test, test_pred))
-# print("[MF] test auc:", auc_mf)
-# user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
-# gi,gu = gini_index(user_wise_ctr)
-# print("***"*5 + "[MF]" + "***"*5)
-
-
-# "MF VITA"
-# mf_vita = MF_VITA(num_user, num_item)
-# mf_vita.fit(x_train, y_train, 
-#     lr=0.01,
-#     batch_size=2048,
-#     lamb=1e-5,
-#     alpha=0.1,
-#     tol=1e-6,
-#     verbose=True)
-
-# test_pred = mf_vita.predict(x_test)
-# mse_mf = mse_func(y_test, test_pred)
-# auc_mf = roc_auc_score(y_test, test_pred)
-# print("***"*5 + "[MF-VITA]" + "***"*5)
-# print("[MF-VITA] test mse:", mse_func(y_test, test_pred))
-# print("[MF-VITA] test auc:", auc_mf)
-# user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
-# gi,gu = gini_index(user_wise_ctr)
-# print("***"*5 + "[MF-VITA]" + "***"*5)
-
-
-# "MF VITA"
-# mf_vita = MF_VITA(num_user, num_item)
-
-# # This parameter is for the COAT.
-# mf_vita.fit(x_train, y_train, 
-#     lr=0.01,
-#     batch_size=128,
-#     lamb=1e-4,
-#     alpha=0.1,
-#     tol=1e-5,
-#     verbose=True)
-
-# mf_vita.fit(x_train, y_train, 
-#     lr=0.01,
-#     batch_size=128,
-#     lamb=1e-4,
-#     alpha=0.1,
-#     tol=1e-5,
-#     verbose=True)
-
-# This is for YAHOO MF-VITA
-# mf_vita.fit(x_train, y_train,
-#     lr=0.01,
-#     batch_size=2048,
-#     lamb=1e-5,
-#     alpha=0.1,
-#     tol=1e-6,
-#     verbose=True)
-
-# This is for YAHOO MF-Naive
-# mf_vita.fit(x_train, y_train, 
-#     lr=0.01,
-#     batch_size=2048,
-#     lamb=1e-5,
-#     alpha=0.0,
-#     tol=1e-6,
-#     verbose=True)
-
-# test_pred = mf_vita.predict(x_test)
-# mse_mf = mse_func(y_test, test_pred)
-# auc_mf = roc_auc_score(y_test, test_pred)
-# print("***"*5 + "[MF-VITA]" + "***"*5)
-# print("[MF-VITA] test mse:", mse_func(y_test, test_pred))
-# print("[MF-VITA] test auc:", auc_mf)
-# user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
-# gi,gu = gini_index(user_wise_ctr)
-# print("***"*5 + "[MF-VITA]" + "***"*5)
-
-# "MF-SNIPS"
-# mf_snips = MF_SNIPS(num_user, num_item)
+# "NCF IPS"
+# ncf_ips = NCF_IPS(num_user, num_item)
 
 # ips_idxs = np.arange(len(y_test))
 # np.random.shuffle(ips_idxs)
 # y_ips = y_test[ips_idxs[:int(0.05 * len(ips_idxs))]]
 
-# mf_snips.fit(x_train, y_train, y_ips=y_ips, lr=0.05, lamb=1e-4,verbose=0)
-# test_pred = mf_snips.predict(x_test)
-# mse_mfsnips = mse_func(y_test, test_pred)
-# auc_mfsnips = roc_auc_score(y_test, test_pred)
-# print("***"*5 + "[MF-SNIPS]" + "***"*5)
-# print("[MF-SNIPS] test mse:", mse_mfsnips)
-# print("[MF-SNIPS] test auc:", auc_mfsnips)
+# ncf_ips.fit(x_train, y_train, 
+#     y_ips=y_ips,
+#     lr=0.01,
+#     batch_size=2048,
+#     lamb=1e-4,tol=1e-6, verbose=1)
+
+# test_pred = ncf_ips.predict(x_test)
+# mse_ncfips = mse_func(y_test, test_pred)
+# auc_ncfips = roc_auc_score(y_test, test_pred)
+# print("***"*5 + "[NCF-IPS]" + "***"*5)
+# print("[NCF-IPS] test mse:", mse_ncfips)
+# print("[NCF-IPS] test auc:", auc_ncfips)
 # user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
 # gi,gu = gini_index(user_wise_ctr)
-# print("***"*5 + "[MF-SNIPS]" + "***"*5)
+# print("***"*5 + "[NCF-IPS]" + "***"*5)
+
+# "NCF SNIPS"
+# ncf_snips = NCF_SNIPS(num_user, num_item)
+
+# ips_idxs = np.arange(len(y_test))
+# np.random.shuffle(ips_idxs)
+# y_ips = y_test[ips_idxs[:int(0.05 * len(ips_idxs))]]
+
+# ncf_snips.fit(x_train, y_train, 
+#     y_ips=y_ips,
+#     lr=0.01,
+#     batch_size=2048,
+#     lamb=1e-4,tol=1e-6, verbose=1)
+
+# test_pred = ncf_snips.predict(x_test)
+# mse_ncfips = mse_func(y_test, test_pred)
+# auc_ncfips = roc_auc_score(y_test, test_pred)
+# print("***"*5 + "[NCF-SNIPS]" + "***"*5)
+# print("[NCF-SNIPS] test mse:", mse_ncfips)
+# print("[NCF-SNIPS] test auc:", auc_ncfips)
+# user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
+# gi,gu = gini_index(user_wise_ctr)
+# print("***"*5 + "[NCF-SNIPS]" + "***"*5)
+
+"NCF VITA"
+ncf_vita = NCF_VITA(num_user, num_item)
+ncf_vita.fit(x_train, y_train, lr=0.01, 
+    alpha=1e-3, gamma=1e-3, lamb=1e-4, tol=1e-6, 
+    batch_size = 2048, verbose=1)
+
+test_pred = ncf_vita.predict(x_test)
+mse_ncf_vita = mse_func(y_test, test_pred)
+auc_ncf_vita = roc_auc_score(y_test, test_pred)
+print("***"*5 + "[NCF-VITA]" + "***"*5)
+print("[NCF] test mse:", mse_func(y_test, test_pred))
+print("[NCF] test auc:", auc_ncf_vita)
+user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
+gi,gu = gini_index(user_wise_ctr)
+print("***"*5 + "[NCF-VITA]" + "***"*5)
+
+
+# "NCF naive"
+# ncf = NCF(num_user, num_item)
+# ncf.fit(x_train, y_train, lr=0.01, lamb=1e-4, tol=1e-6, 
+#     batch_size = 2048, verbose=1)
+# test_pred = ncf.predict(x_test)
+# mse_ncf = mse_func(y_test, test_pred)
+# auc_ncf = roc_auc_score(y_test, test_pred)
+# print("***"*5 + "[NCF]" + "***"*5)
+# print("[NCF] test mse:", mse_func(y_test, test_pred))
+# print("[NCF] test auc:", auc_ncf)
+# user_wise_ctr = get_user_wise_ctr(x_test,y_test,test_pred)
+# gi,gu = gini_index(user_wise_ctr)
+# print("***"*5 + "[NCF]" + "***"*5)
+
 
 pdb.set_trace()
 
